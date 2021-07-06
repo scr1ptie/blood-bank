6526f7c90a82e5ae946fd719fe82f6355d0010e9
Scriptie <33485136+scr1ptie@users.noreply.github.com>
2021-07-06 10:23:40 +0530

    Create store.php

---
+++ blood-bank/store.php
@@ -0,0 +1,39 @@
+<?php
+ 
+$servername = "localhost";
+$username = "blood";
+$password = "blood";
+$dbname = "blood";
+ 
+// Create connection
+$conn = new mysqli($servername,
+    $username, $password, $dbname);
+ 
+if($conn === false){
+    die("ERROR: Could not connect. "
+        . mysqli_connect_error());
+}
+
+$city =  $_REQUEST['city'];
+$bg = $_REQUEST['bg'];
+$age =  $_REQUEST['age'];
+$hospital = $_REQUEST['hospital'];
+$reason = $_REQUEST['reason'];
+
+$sql = "INSERT INTO req VALUES ('$city',
+            '$bg','$age','$hospital','$reason')";
+
+if(mysqli_query($conn, $sql)){
+    echo "<h3>data stored in a database successfully."
+        . " browse your localhost php my admin"
+        . " to view the updated data</h3>";
+
+} else{
+    echo "ERROR: Sorry $sql. "
+        . mysqli_error($conn);
+}
+ 
+// Close connection
+mysqli_close($conn);
+
+?>
